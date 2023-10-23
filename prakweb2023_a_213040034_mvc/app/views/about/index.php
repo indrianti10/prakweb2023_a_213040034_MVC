bf2542376cf7eedcd59bd7e5fd8678f058316140
indrianti10 <94700600+indrianti10@users.noreply.github.com>
2023-10-23 23:27:02 +0700

    Add files via upload
    pertemuan6

---
+++ prakweb2023_a_213040034_mvc/app/views/about/index.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <h1 class="mt-4">About me</h1>
+    <img src="<?= BASEURL; ?>/img/indri.jpg" alt="Indri" width="200" class="rounded-circle shadow">
+    <p>Halo, nama saya <?=$data['nama']; ?>, umur saya <?=$data['umur']; ?> tahun,
+     Saya adalah seorang <?=$data['pekerjaan']; ?></p>
+</div>
